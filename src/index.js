809c8b6c731cdf24f8f1cf47e61e27cf17233f74
EvgenPrushk <ogmprushak@mail.ru>
2018-10-08 16:48:12 +0300

    task 9 rdy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+ /*module variables*/
+let counter = 0;
+let j = 0;
+const longestConsecutive = new Set(array);
+/*determine the maximum and minimum numbers in the array */
+let minnumberArray = Math.min.apply(Math, array);
+let maxnumberArray = Math.max.apply(Math, array);
+/*min and max  in Array */
+for (let i = minnumberArray; i <= maxnumberArray; i++ ) {
+/*if i has  longestConsecutive*/
+  if (longestConsecutive.has(i)){
+    j++;
+    /* return counter */
+    if (j > counter){
+      counter = j;
+    }
+    
+  } else j = 0;
+
 }
+ return counter
+ }
